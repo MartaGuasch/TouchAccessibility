4a08b6f83ccfee5ac941c73f94c1b7e98baef5ac
Marta Guasch <martaguaschprieto@gmail.com>
2014-05-14 19:23:02 +0200

    Home button drown but it doesn't work

--- src/com/example/accessibility/ListenerView.java
+++ src/com/example/accessibility/ListenerView.java
@@ -12,14 +12,22 @@ import android.graphics.Point;
 import android.view.ViewGroup;
 
 public class ListenerView extends ViewGroup{
-    Path tRight= new Path();
+    Path triangle= new Path();
     Path tLeft= new Path();
     Path tTop= new Path();
     Path tBottom= new Path();
     
+    Paint paintBlack = new Paint();
+	Paint paintWhite = new Paint();
+	
+	Point a = new Point();
+	Point b = new Point();
+	Point c = new Point();
+    
 	public ListenerView(Context context) {
 		super(context);
-        paint.setColor(Color.BLACK);
+        paintBlack.setColor(Color.BLACK);
+        paintWhite.setColor(Color.WHITE);
        
 		// TODO Auto-generated constructor stub
 	}
@@ -34,7 +42,7 @@ public class ListenerView extends ViewGroup{
 		
 		
 	}
-	Paint paint = new Paint();
+	
 	/*
 	public void onDrawNodeBorder(AccessibilityNodeInfoCompat node, int color){
 			Log.i("prints","entra al onDrawNodeBorder");
@@ -86,75 +94,105 @@ public class ListenerView extends ViewGroup{
     	/*paint.setColor(android.graphics.Color.BLACK);
         canvas.drawPaint(paint);
         */
-        paint.setStrokeWidth(4);
-        paint.setColor(android.graphics.Color.WHITE);
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
-        paint.setAntiAlias(true);
+        paintWhite.setStrokeWidth(4);
+        paintWhite.setARGB(128,255,255,255);
+        paintWhite.setStyle(Paint.Style.FILL_AND_STROKE);
+        paintWhite.setAntiAlias(true);
+        
+        paintBlack.setStrokeWidth(4);
+        paintBlack.setARGB(128,0,0,0);
+        paintBlack.setStyle(Paint.Style.FILL_AND_STROKE);
+        paintBlack.setAntiAlias(true);
         
         int left = getPaddingLeft();
         int top = getPaddingTop();
         int right =  getWidth() - getPaddingRight();
         int bottom = getHeight() - getPaddingBottom();
         
+        
        //canvas.drawLine(left, top, right, bottom, paint);
         
-        canvas.drawRect(right-80, bottom/2+80, right, bottom/2, paint);
-        canvas.drawRect(left, bottom/2+80, left+80, bottom/2, paint);
-        canvas.drawRect(right/2-40, bottom-90, right/2+40, bottom-10, paint);
-        canvas.drawRect(right/2-40, top+30, right/2+40, top+110, paint);
+        //-----------------------------------------------------------------------------//
+        //Drawing general buttons
+        canvas.drawRect(right-80, bottom-90, right, bottom-10, paintWhite);
+        canvas.drawRect(right-70, bottom-55, right-10, bottom-20, paintBlack);
         
-        //Drowing triangle (right arrow)
-        paint.setColor(android.graphics.Color.BLACK);
-        Point aL = new Point(right-70, bottom/2+70);
-        Point bL = new Point(right-70, bottom/2+10);
-        Point cL = new Point(right-10, bottom/2+40);
-        tRight.setFillType(FillType.EVEN_ODD);
-        tRight.moveTo(aL.x, aL.y);
-        tRight.lineTo(bL.x, bL.y);
-        tRight.lineTo(cL.x,cL.y);
-        tRight.lineTo(aL.x, aL.y);
-        tRight.close();
-        canvas.drawPath(tRight, paint);
-        
-        
-        //Drowing triangle (left arrow)
-        paint.setColor(android.graphics.Color.BLACK);
-        Point aR =new Point (left+70, bottom/2+70);
-        Point bR = new Point(left+70, bottom/2+10);
-        Point cR = new Point(left+10, bottom/2+40);
-        tLeft.setFillType(FillType.EVEN_ODD);
-        tLeft.moveTo(aR.x, aR.y);
-        tLeft.lineTo(bR.x, bR.y);
-        tLeft.lineTo(cR.x, cR.y);
-        tLeft.lineTo(aR.x, aR.y);
+        a.set(right-70, bottom-55);
+        b.set(right-10, bottom-55);
+        c.set(right-40, bottom-80);
+        tLeft.moveTo(a.x, a.y);
+        tLeft.lineTo(b.x, b.y);
+        tLeft.lineTo(c.x, c.y);
+        tLeft.lineTo(a.x, a.y);
         tLeft.close();
-        canvas.drawPath(tLeft, paint);
+        canvas.drawPath(tLeft, paintBlack);
         
-        //Drowing triangle (top arrow)
-        paint.setColor(android.graphics.Color.BLACK);
-        Point aT = new Point(right/2-30, top+100);
-        Point bT = new Point(right/2, top+40);
-        Point cT = new Point(right/2+30, top+100);
-        tRight.setFillType(FillType.EVEN_ODD);
-        tRight.moveTo(aT.x, aT.y);
-        tRight.lineTo(bT.x, bT.y);
-        tRight.lineTo(cT.x,cT.y);
-        tRight.lineTo(aT.x, aT.y);
-        tRight.close();
-        canvas.drawPath(tRight, paint);
         
-        //Drowing triangle (bottom arrow)
-        paint.setColor(android.graphics.Color.BLACK);
-        Point aB =new Point (right/2-30, bottom-80);
-        Point bB = new Point(right/2, bottom-20);
-        Point cB = new Point(right/2+30, bottom-80);
-        tLeft.setFillType(FillType.EVEN_ODD);
-        tLeft.moveTo(aB.x, aB.y);
-        tLeft.lineTo(bB.x, bB.y);
-        tLeft.lineTo(cB.x, cB.y);
-        tLeft.lineTo(aB.x, aB.y);
+        //-----------------------------------------------------------------------------//
+        //Drawing scroll buttons
+        
+        //Drawing the squares
+        canvas.drawRect(right-80, bottom/2+80, right, bottom/2, paintWhite);
+        canvas.drawRect(left, bottom/2+80, left+80, bottom/2, paintWhite);
+        canvas.drawRect(right/2-40, bottom-90, right/2+40, bottom-10, paintWhite);
+        canvas.drawRect(right/2-40, top+30, right/2+40, top+110, paintWhite);
+        
+        //Drawing triangle (right arrow)
+        a.set(right-70, bottom/2+70);
+        b.set(right-70, bottom/2+10);
+        c.set(right-10, bottom/2+40);
+        //tRight.setStyle(Paint.Style.FILL_AND_STROKE);
+        triangle.moveTo(a.x, a.y);
+        triangle.lineTo(b.x, b.y);
+        //tRight.moveTo(bL.x, bL.y);
+        triangle.lineTo(c.x,c.y);
+        //tRight.moveTo(cL.x,cL.y);
+        triangle.lineTo(a.x, a.y);
+        triangle.close();
+        canvas.drawPath(triangle, paintBlack);
+        
+        
+        //Drawing triangle (left arrow)
+        a.set(left+70, bottom/2+70);
+        b.set(left+70, bottom/2+10);
+        c.set(left+10, bottom/2+40);
+        //tLeft.setFillType(FillType.EVEN_ODD);
+        tLeft.moveTo(a.x, a.y);
+        tLeft.lineTo(b.x, b.y);
+        //tLeft.moveTo(bR.x, bR.y);
+        tLeft.lineTo(c.x, c.y);
+        //tLeft.moveTo(cR.x, cR.y);
+        tLeft.lineTo(a.x, a.y);
         tLeft.close();
-        canvas.drawPath(tLeft, paint);
+        canvas.drawPath(tLeft, paintBlack);
+        
+        //Drawing triangle (top arrow)
+        a.set(right/2-30, top+100);
+        b.set(right/2, top+40);
+        c.set(right/2+30, top+100);
+        //tRight.setFillType(FillType.EVEN_ODD);
+        triangle.moveTo(a.x, a.y);
+        triangle.lineTo(b.x, b.y);
+        //tRight.moveTo(bT.x, b.y);
+        triangle.lineTo(c.x,c.y);
+        //tRight.moveTo(cT.x,cT.y);
+        triangle.lineTo(a.x, a.y);
+        triangle.close();
+        canvas.drawPath(triangle, paintBlack);
+        
+        //Drawing triangle (bottom arrow)
+        a.set(right/2-30, bottom-80);
+        b.set(right/2, bottom-20);
+        c.set(right/2+30, bottom-80);
+        //tLeft.setFillType(FillType.EVEN_ODD);
+        tLeft.moveTo(a.x, a.y);
+        tLeft.lineTo(b.x, b.y);
+        //tLeft.moveTo(bB.x, bB.y);
+        tLeft.lineTo(c.x, c.y);
+        //tLeft.moveTo(cB.x, cB.y);
+        tLeft.lineTo(a.x, a.y);
+        tLeft.close();
+        canvas.drawPath(tLeft, paintBlack);
         
         /*
         //Dibuixem el triangle de la dreta (per fer scroll dreta)
